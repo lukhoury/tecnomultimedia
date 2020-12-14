eaa26e13776ef191c0e82ed0ad924375649062cc
lukhoury <66232483+lukhoury@users.noreply.github.com>
2020-12-14 19:10:34 -0300

    Add files via upload

---
+++ TP2/js/inicia.js
@@ -0,0 +1,19 @@
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
+
+    $(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
+    $('.carousel.carousel-slider').carousel({
+  fullWidth: true
+});
+/*PRUEBAS */
